a415ff93bca953c598439d98cf9532fdf83ccd77
norazics <nora.zics@yahoo.co.uk>
2023-01-06 14:05:56 +0000

    js-2darray

--- files/script.js
+++ files/script.js
@@ -4,9 +4,7 @@ console.log('lets do this!');
 let playingField = [ 
                     ['0', '1', '2'], 
                     ['3', '4', '5'], 
-                    ['6', '7', '8']
-                    
-];
+                    ['6', '7', '8']];
 
 console.log(playingField[0])
 console.log(playingField[1])
