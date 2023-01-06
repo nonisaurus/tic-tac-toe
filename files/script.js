0680a631f456f28264a94c8122ab21c7fee84d08
norazics <nora.zics@yahoo.co.uk>
2023-01-06 14:01:11 +0000

    working-on-js

---
+++ files/script.js
@@ -0,0 +1,28 @@
+console.log('lets do this!');
+
+// declairing stuff
+let playingField = [ 
+                    ['0', '1', '2'], 
+                    ['3', '4', '5'], 
+                    ['6', '7', '8']
+                    
+];
+
+console.log(playingField[0])
+console.log(playingField[1])
+console.log(playingField[2])
+
+// creating 2d array 
+for (let i = 0; i < playingField.length; i++) {
+    for ( let u = 0; u < playingField[i].length; u++) {
+        playingField[i][u]
+    }
+}
+
+console.log(playingField[2][2])
+console.log(playingField)
+
+
+
+
+
