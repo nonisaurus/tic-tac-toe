4e86241942a61e3a5ab769f05aea7741befdbbf4
norazics <nora.zics@yahoo.co.uk>
2023-01-11 15:50:01 +0000

    responsive-screen-size-plus-tidy-up

--- files/script.js
+++ files/script.js
@@ -1,8 +1,8 @@
 console.log('lets do this!');
 
 // declaring and grabbings stuff from html
-const restartBtn = document.querySelector('.restart-btn');
-const startBtn = document.querySelector('.start-btn')
+const startBtn = document.querySelector('.restart-btn');
+// const startBtn = document.querySelector('.start-btn')
 const tokenChoiceBtn = document.querySelector('.choose-btn');
 const aiBtn = document.querySelector('.ai-btn');
 
@@ -15,6 +15,8 @@ const extraTokenNodeList = document.querySelector('.extra-tokens')
 const musicBox = document.querySelector('.music')
 let musicBtn = document.querySelector('.music-btn')
 
+
+// global variables
 let currentShape = "circle";
 let isWinner = false;
 let isPlaying = false;
@@ -35,14 +37,65 @@ const winningComboIndex = [
 ];
 
 
-
-
 // nodelist to an array
 const playingFieldSquares = Array.prototype.slice.call(squaresNodeList);
 const extraTokensArray = Array.prototype.slice.call(extraTokenNodeList)
 
-// toggle between two functions
-const toggleTurns = (element) => {
+
+
+
+
+// GAME 
+const game = () => {
+    playingFieldSquares.forEach(
+        function (element) {
+            // removes classes so it clears fields
+            element.classList.remove('circle', 'cross')
+
+            element.onclick = () => {
+                // saving input to empty variables
+                currentShape === 'circle' ? circleTokens.push(playingFieldSquares.indexOf(element)) : crossTokens.push(playingFieldSquares.indexOf(element)) 
+
+                // calling winning
+                isWinner = determineWinner(currentShape);
+                
+                // taking turns if there is no winner
+                !isWinner && toggleTurns(element)
+                
+                // display winner
+                displayWinner()
+                
+                // remove click event on current element
+                element.onclick = null
+                
+                console.log('this is current shape', currentShape)
+            };
+        }
+    )
+}
+
+// check if tokens numbers match winning numbers
+const determineWinner = (currentShape) => {
+
+    circleORcrossArray = currentShape === 'circle' ? circleTokens : crossTokens
+
+    if (circleORcrossArray.length >= 3){
+        // looping through every winningcombo arrays (some takes a function)
+        const hasWon = winningComboIndex.some(winningOptionArr => {
+           // checking each array one by one by their index (every takes a function)
+            return winningOptionArr.every(winningIndex => {
+                // if this winning index includes circleOrcross number then return boolean (includes does NOT take a function)
+                return circleORcrossArray.includes(winningIndex)
+           })
+        }) 
+        return hasWon ? currentShape : false 
+    } 
+    return false 
+}
+
+
+// toggle between two functions =CHANGE put to bottom
+function toggleTurns(element) {
     if ( currentShape === "circle" ){
         circleFunction(element);
         currentShape = "cross"
@@ -55,14 +108,14 @@ const toggleTurns = (element) => {
 
 
 // change background to circle image
-const circleFunction = (element) => {
+function circleFunction (element) {
     element.classList.add('circle')
     displayWhosTurn("Pink, it's your turn!")
 }
 
 
 // change background to cross img
-const crossFunction = (element) => {
+function crossFunction (element) {
     element.classList.add('cross')
     displayWhosTurn("Purple, it's your turn")
 
@@ -70,84 +123,34 @@ const crossFunction = (element) => {
 
 
 // display whos turn it is
-const displayWhosTurn = (content) => {
+function displayWhosTurn (content) {
     paragraphHeader.innerHTML = content
     return paragraphHeader.innerHTML
 } 
 
 
-// GAME 
-const game = () => {
-    playingFieldSquares.forEach(
-        function (element) {
-            // removes classes so it clears fields
-            element.classList.remove('circle', 'cross')
-            element.onclick = () => {
-                // saving input to empty variables
-                currentShape === 'circle' ? circleTokens.push(playingFieldSquares.indexOf(element)) : crossTokens.push(playingFieldSquares.indexOf(element)) 
-
-                // calling winning
-                isWinner = determineWinner(currentShape);
-
-                // display winner
-                displayWinner()
-
-                // taking turns
-                toggleTurns(element)
-
-                // remove click event on current element
-                element.onclick = null
-
-                console.log('this is current shape', currentShape)
-            };
-        }
-    )
-}
-
-
-// check if tokens numbers match winning numbers
-const determineWinner = (currentShape) => {
-    circleORcrossArray = currentShape === 'circle' ? circleTokens : crossTokens
-
-    if (circleORcrossArray.length >= 3){
-        // looping through every winningcombo arrays
-        const hasWon = winningComboIndex.some(winningOptionArr => {
-           // checking each array one by one > index 
-            return winningOptionArr.every(winningIndex => {
-                // is this winning index includes circleOrcross return boolean
-                return circleORcrossArray.includes(winningIndex)
-           })
-        }) 
-        return hasWon ? currentShape : false 
-    } 
-    return false 
-}
-
-
 // display winner
 const displayWinner = () => {
      if (isWinner){
-        
-        paragraphHeader.innerHTML = 'Press RESTART'
-            
-        removeAdVisible(winningMessage, mainElement)
-
-        currentShape === 'circle' ? winningMessage.innerHTML = 'Purple Won' : winningMessage.innerHTML = 'Pink Won'
-
+         removeAdVisible(winningMessage, mainElement)
+         
+         currentShape === 'circle' ? winningMessage.innerHTML = 'Purple Won' : winningMessage.innerHTML = 'Pink Won'
+         
+         paragraphHeader.innerHTML = 'Press RESTART'
         
     } else if ((circleTokens.length || crossTokens.length) === 5) { 
-        paragraphHeader.innerHTML = 'Press RESTART'
-
+        
         winningMessage.innerHTML = 'Its a Tie'
-
+        
         removeAdVisible(winningMessage, mainElement)
         
+        paragraphHeader.innerHTML = 'Press RESTART'
     } 
 }
 
 
 // remove invisibility class from first and add it to second else the opposite 
-const removeAdVisible = (removeFirst, addFirst) => {
+function removeAdVisible (removeFirst, addFirst) {
     if (removeFirst.classList.contains('visibility-none')){
         removeFirst.classList.remove('visibility-none')
         addFirst.classList.add('visibility-none')
@@ -159,10 +162,10 @@ const removeAdVisible = (removeFirst, addFirst) => {
 
 
 // reset page to original
-const resetPage = () => {
-    winningMessage.classList.contains('visibility-none') ? false : winningMessage.classList.add('visibility-none')
+function resetPage () {
+    !winningMessage.classList.contains('visibility-none') && winningMessage.classList.add('visibility-none')
 
-    mainElement.classList.contains('visibility-none') ? mainElement.classList.remove('visibility-none') : false
+    mainElement.classList.contains('visibility-none') && mainElement.classList.remove('visibility-none')
 
     tokenChoices.classList.add('visibility-none')
 }
@@ -180,37 +183,15 @@ extraTokensArray.forEach(
 
 console.log(specialToken)
 
-// toggle for music
-function toggleMusic () {
-    return musicBox.paused ? musicBox.play(musicBtn.innerHTML = "STOP PLAY") : musicBox.pause(musicBtn.innerHTML = "START PLAY");
-}
-
-musicBtn.onclick = () => {toggleMusic()}
-
-
-
-
-
-
-
 
 
 
 
 // BUTTONS
-
-
-// start button
-startBtn.onclick = (event) => {
-    event.preventDefault()
-    displayWhosTurn('Purple it is your turn!')
-    game()
-    
-}
-
 // restart button 
-restartBtn.onclick = (event) => {
+startBtn.onclick = (event) => {
     event.preventDefault();
+    startBtn.innerHTML = 'Restart'
     circleTokens = [];
     crossTokens = [];
     currentShape = 'circle';
@@ -219,14 +200,21 @@ restartBtn.onclick = (event) => {
     game()
 }
 
+
 // choose button
 tokenChoiceBtn.onclick = function (event) {
     event.preventDefault()
-
+    
     winningMessage.classList.contains('visibility-none') ? false : winningMessage.classList.add('visibility-none')
-
+    
     removeAdVisible(tokenChoices, mainElement)
 }
 
+
 // music start / stop
+function toggleMusic () {
+    return musicBox.paused ? musicBox.play(musicBtn.innerHTML = "STOP PLAY") : musicBox.pause(musicBtn.innerHTML = "START PLAY");
+}
+
+musicBtn.onclick = () => {toggleMusic()}
 
